7affb8b68c9a7afd3d0d95ea6f273d422dcff781
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-11-19 23:19:44 +0400

    doing first project

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,9 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const body = document.querySelector("body");
+const btn = document.querySelector('#btn');
+
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  body.style.backgroundColor = color;
+})
